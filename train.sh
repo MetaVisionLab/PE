2b90612c58e7eb8117062cf498b8d3bcfb9fa17b
Shen Zhifeng <shen_zhifeng@outlook.com>
2023-10-17 20:13:01 +0800

    update README.md

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ then
   python3 main.py $cartoon_cfg --dymodel $1 --pe_type $2
   art_painting_cfg="--trainer DRT_DG_Mixup --source-domains sketch photo cartoon --target-domains art_painting --output-dir drt_dg_r/pacs_resnet50_draac_v3_strong_transform_1/pacs/art_painting/1 --dataset-config-file configs/datasets/drt_dg_r/pacs_resnet50_draac_v3_strong_transform_1.yaml --config-file configs/trainers/drt_dg_r/pacs.yaml --seed 2022 "
   python3 main.py $art_painting_cfg --dymodel $1 --pe_type $2
-  python3 parse_test_res.py "exprs/$1_$2/drt_dg_r/pacs_resnet50_draac_v3_strong_transform_1/pacs" --last-five --multi-exp
+  python3 parse_test_res.py "exprs/$2_$1/drt_dg_r/pacs_resnet50_draac_v3_strong_transform_1/pacs" --last-five --multi-exp
 elif [ $3 = "OfficeHome" ]
 then
   art_cfg="--trainer DRT_DG_Mixup --source-domains clipart product real_world --target-domains art --output-dir drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2/office_home/art/1 --dataset-config-file configs/datasets/drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2.yaml --config-file configs/trainers/drt_dg_r/office_home_dg.yaml --seed 2022 "
@@ -20,7 +20,7 @@ then
   python3 main.py $product_cfg --dymodel $1 --pe_type $2
   real_world_cfg="--trainer DRT_DG_Mixup --source-domains art clipart product --target-domains real_world --output-dir drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2/office_home/real_world/1 --dataset-config-file configs/datasets/drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2.yaml --config-file configs/trainers/drt_dg_r/office_home_dg.yaml --seed 2022 "
   python3 main.py $real_world_cfg --dymodel $1 --pe_type $2
-  python3 parse_test_res.py "exprs/$1_$2/drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2/office_home" --last-five --multi-exp
+  python3 parse_test_res.py "exprs/$2_$1/drt_dg_r/office_home_dg_resnet50_draac_v3_strong_transform_2/office_home" --last-five --multi-exp
 elif [ $3 = "VLCS" ]
 then
   caltech_cfg="--trainer DRT_DG_Mixup --source-domains labelme pascal sun --target-domains caltech --output-dir drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2/vlcs/caltech/1 --dataset-config-file configs/datasets/drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2.yaml --config-file configs/trainers/drt_dg_r/vlcs.yaml --seed 2022 "
@@ -31,7 +31,7 @@ then
   python3 main.py $pascal_cfg --dymodel $1 --pe_type $2
   sun_cfg="--trainer DRT_DG_Mixup --source-domains caltech labelme pascal --target-domains sun --output-dir drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2/vlcs/sun/1 --dataset-config-file configs/datasets/drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2.yaml --config-file configs/trainers/drt_dg_r/vlcs.yaml --seed 2022 "
   python3 main.py $sun_cfg --dymodel $1 --pe_type $2
-  python3 parse_test_res.py "exprs/$1_$2/drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2/vlcs" --last-five --multi-exp
+  python3 parse_test_res.py "exprs/$2_$1/drt_dg_r/vlcs_resnet50_draac_v3_strong_transform2/vlcs" --last-five --multi-exp
 elif [ $3 = "TerriaIncognita" ]
 then
   location_38_cfg="--trainer DRT_DG_Mixup --source-domains location_43 location_46 location_100 --target-domains location_38 --output-dir drt_dg_r/DRT_DG_Mixup_dg_resnet50_draac_v3_strong_transform5/incognita_dg/location_38/1 --dataset-config-file configs/datasets/drt_dg_r/terra_incognita_dg_resnet50_draac_v3_strong_transform5.yaml --config-file configs/trainers/drt_dg_r/terra_incognita_dg.yaml --seed 2022 "
@@ -42,7 +42,7 @@ then
   python3 main.py $location_46_cfg --dymodel $1 --pe_type $2
   location_100_cfg="--trainer DRT_DG_Mixup --source-domains location_38 location_43 location_46 --target-domains location_100 --output-dir drt_dg_r/DRT_DG_Mixup_dg_resnet50_draac_v3_strong_transform5/incognita_dg/location_100/1 --dataset-config-file configs/datasets/drt_dg_r/terra_incognita_dg_resnet50_draac_v3_strong_transform5.yaml --config-file configs/trainers/drt_dg_r/terra_incognita_dg.yaml --seed 2022 "
   python3 main.py $location_100_cfg --dymodel $1 --pe_type $2
-  python3 parse_test_res.py "exprs/$1_$2/ddrt_dg_r/DRT_DG_Mixup_dg_resnet50_draac_v3_strong_transform5/incognita_dg" --last-five --multi-exp
+  python3 parse_test_res.py "exprs/$2_$1/ddrt_dg_r/DRT_DG_Mixup_dg_resnet50_draac_v3_strong_transform5/incognita_dg" --last-five --multi-exp
 elif [ $3 = "DomainNet" ]
 then
   clipart_cfg="--trainer DRT_DG_Mixup --source-domains infograph painting quickdraw real sketch --target-domains clipart --output-dir drt_dg_r/domainnet_resnet50_draac_v3/domainnet/clipart/1 --dataset-config-file configs/datasets/drt_dg_r/domainnet_resnet50_draac_v3.yaml --config-file configs/trainers/drt_dg_r/domainnet.yaml --seed 2022 "
@@ -57,7 +57,7 @@ then
   python3 main.py $real_cfg --dymodel $1 --pe_type $2
   sketch_cfg="--trainer DRT_DG_Mixup --source-domains clipart infograph painting quickdraw real --target-domains sketch --output-dir drt_dg_r/domainnet_resnet50_draac_v3/domainnet/sketch/1 --dataset-config-file configs/datasets/drt_dg_r/domainnet_resnet50_draac_v3.yaml --config-file configs/trainers/drt_dg_r/domainnet.yaml --seed 2022 "
   python3 main.py $sketch_cfg --dymodel $1 --pe_type $2
-  python3 parse_test_res.py "exprs/$1_$2/drt_dg_r/domainnet_resnet50_draac_v3/domainnet" --last-five --multi-exp
+  python3 parse_test_res.py "exprs/$2_$1/drt_dg_r/domainnet_resnet50_draac_v3/domainnet" --last-five --multi-exp
 else
   echo 'The dataset arg should be one of "PACS", "VLCS", "OfficeHome", "TerriaIncognita", or "DomainNet"'
   echo "Usage: bash train.sh {arg1=dymodel} {arg2=pe_type} {arg3=dataset}"
